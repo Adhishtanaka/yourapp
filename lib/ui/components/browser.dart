e85e6712bdf4a285f0a7c61a5806aac6b7a481ff
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2025-02-24 17:06:21 +0530

    fixed bug

--- lib/ui/components/browser.dart
+++ lib/ui/components/browser.dart
@@ -12,23 +12,37 @@ class BrowserUI extends StatefulWidget {
 
 class _BrowserUIState extends State<BrowserUI> {
   InAppWebViewController? webViewController;
+  late PullToRefreshController pullToRefreshController;
+
+  String cleanHtml(String html) {
+    String cleaned = html.replaceAll('```html', '').replaceAll('```', '').trim();
+    return cleaned;
+  }
 
   @override
   void initState() {
     super.initState();
-    webViewController?.loadData(data: widget.html);
+    pullToRefreshController = PullToRefreshController(
+      settings: PullToRefreshSettings(color: Colors.black),
+      onRefresh: () {
+        webViewController?.reload();
+      },
+    );
   }
-
-  PullToRefreshController pullToRefreshController = PullToRefreshController(
-      settings: PullToRefreshSettings(color: Colors.black));
 
   @override
   Widget build(BuildContext context) {
     return InAppWebView(
+      initialData: InAppWebViewInitialData(
+        data: cleanHtml(widget.html),
+        baseUrl: WebUri('https://localhost'),
+      ),
       pullToRefreshController: pullToRefreshController,
       onWebViewCreated: (controller) {
         webViewController = controller;
-        webViewController?.loadData(data: widget.html);
+      },
+      onLoadStop: (controller, url) {
+        pullToRefreshController.endRefreshing();
       },
     );
   }
